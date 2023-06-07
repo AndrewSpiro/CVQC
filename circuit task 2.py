68395cf555db993531071399784bb60224182d6f
AndrewSpiro <77333606+AndrewSpiro@users.noreply.github.com>
2023-06-07 13:13:24 +0200

    Changed the for loop so that the loss is the circuit output

--- circuit task 2.py
+++ circuit task 2.py
@@ -6,6 +6,7 @@ from keras.optimizers import Adam
 
 t = np.linspace(0,10,1000)
 X = np.sin(t)
+print(X[50])
 
 dev = qml.device('default.qubit', wires=3)
 
@@ -19,7 +20,7 @@ def circuit(x,y,weights): # y is going to be X(i+1) while x is X(i)
     qml.Hadamard(wires=[0])  # performing the cswap test- i.e. measuring the difference between wire 1 and 2
     qml.CSWAP([0, 1, 2])
     qml.Hadamard(wires=[0])
-    return qml.expval(qml.PauliZ(0))  # This is going to return either a 1 (states are different) or a 0 (states are the same)
+    return qml.expval(qml.PauliZ(0))  # Returns a value between 0 and 1. If 0, the states are the same.
 
 weights = np.random.uniform(0,pi,3)
 
@@ -32,13 +33,9 @@ loss = 1
 for i in range(epochs):
     print(loss)
     for j in range(len(X)-1):  # Computing an comparing Ux(t) with x(t+1) for all t
-        M = []
-        for k in range(swap_accuracy):
-            print("X[j] is" + str(X[j]) + "and X[j + 1] is " + str(X[j+1]))
-            M.append ( circuit( [X[j]], [X[j + 1]], weights) )
-        s = 1 - 2/len(swap_accuracy) * sum(M)  # M is 0 when they are the same, so s will equal 1.
+        loss = circuit(X(j), X(j+1), weights)
         with tf.GradientTape() as tape:
-            loss = 1 - abs(s) # Loss is 0 when states are the same
+            loss = loss
         gradients = tape.gradient(loss, weights)
         optimizer.apply_gradients(gradients, weights)
 
