eeaed193ee1b10c6a40427ece81511bf7d9ac6d4
AndrewSpiro <77333606+AndrewSpiro@users.noreply.github.com>
2023-06-23 13:17:57 +0200

    Circuit task 2 updates

--- circuit task 2.py
+++ circuit task 2.py
@@ -1,22 +1,21 @@
-import numpy as np
+from pennylane import numpy as np
 import pennylane as qml
 from numpy import pi
-import tensorflow as tf
-from keras.optimizers import Adam
+#import tensorflow as tf
+#from keras.optimizers import Adam
 
-t = np.linspace(0,10,1000)
+t = np.linspace(0,10,100)
 X = np.sin(t)
-print(X[50])
 
 dev = qml.device('default.qubit', wires=3)
 
 @qml.qnode(dev)
-def circuit(x,y,weights): # y is going to be X(i+1) while x is X(i)
-    qml.AngleEmbedding(features = x, wires = [1])  # creating psi(x)
+def circuit(weights,x,y): # y is going to be X(i+1) while x is X(i)
+    qml.AngleEmbedding(features = [x], wires = [1])  # creating psi(x)
     qml.RZ(weights[0], wires=[1])
     qml.RY(weights[1], wires=[1])
     qml.RZ(weights[2], wires=[1])  # creating U(theta)psi(x)
-    qml.AngleEmbedding(y, wires = [2])
+    qml.AngleEmbedding([y], wires = [2])
     qml.Hadamard(wires=[0])  # performing the cswap test- i.e. measuring the difference between wire 1 and 2
     qml.CSWAP([0, 1, 2])
     qml.Hadamard(wires=[0])
@@ -24,18 +23,37 @@ def circuit(x,y,weights): # y is going to be X(i+1) while x is X(i)
 
 weights = np.random.uniform(0,pi,3)
 
-LR = 0.001
-optimizer = Adam(learning_rate=LR)
-epochs = 100
-swap_accuracy = 20  # how many times the swap test is performed for each prediction
-loss = 1
+def cost(weights, x_batch, y_batch):
+    loss = 0
+    for j in range(len(x_batch)):
+        loss += circuit(weights, x_batch[j],y_batch[j])
+    return loss
 
-for i in range(epochs):
-    print(loss)
-    for j in range(len(X)-1):  # Computing an comparing Ux(t) with x(t+1) for all t
-        loss = circuit(X(j), X(j+1), weights)
-        with tf.GradientTape() as tape:
-            loss = loss
-        gradients = tape.gradient(loss, weights)
-        optimizer.apply_gradients(gradients, weights)
+batch_size = 5
+x_batch = np.random.randint(0,len(X),batch_size,)
+y_batch = x_batch + 1
+
+#print(cost(weights, [0.3], [0.2]))  # Testing cost function
+
+LR = 0.1
+#optimizer = Adam(learning_rate=LR)
+opt = qml.AdamOptimizer
+epochs = 100
+cst = []
+
+for steps in range(epochs):
+    weights, c, _ = opt.step_and_cost(cost, weights, x_batch, y_batch)
+    cst.append(c)
+    if steps % 5 == 0:
+        print(cost)
+
+
+#for i in range(epochs):
+#    print(loss)
+    #for j in range(len(X)-1):  # Computing an comparing Ux(t) with x(t+1) for all t
+        #loss = circuit(X(j), X(j+1), weights)
+        #with tf.GradientTape() as tape:
+            #loss = loss # add the loss instead of resetting and make a cost function
+        #gradients = tape.gradient(loss, weights)
+        #optimizer.apply_gradients(gradients, weights)
 
