475d6828c6f0557e787a8739ccaf558f850082e1
AndrewSpiro <spiro.ac@gmail.com>
2023-07-26 16:33:01 +0200

    Copy DataPreprocessing into Functions folder

--- CleanVersion/DataPreprocessing.py
+++ CleanVersion/DataPreprocessing.py
@@ -1,8 +1,8 @@
 # %%
 from DataPreprocessingFuncs import *
 # %%
-dataset = load_data('AAPL max.csv',usecols=['Close'], sample_size=201, bool_plot=True)
-percent_of_change = gradient(dataset, bool_plot=True)
-DC, amp, N = FT_mod_squared(percent_of_change, 1, True, save_components='Apple 200 1')
-full_signal, r = build_signal(DC, amp, 0, 0, N, True)
+dataset = load_data('Datasets/AAPLmax.csv',usecols=['Close'], sample_size=201)
+percent_of_change = gradient(dataset)
+DC, amp, N = FT_mod_squared(percent_of_change, 1)
+full_signal, r = build_signal(DC, amp, 0, 0, N)
 # %%
