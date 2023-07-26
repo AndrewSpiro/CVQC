3a93e0329ddead999771d3edf929b8b1d51f1d35
AndrewSpiro <spiro.ac@gmail.com>
2023-07-26 16:37:56 +0200

    Commit first working Training file

--- CleanVersion/Training.py
+++ CleanVersion/Training.py
@@ -1,6 +1,16 @@
 # %%
-from Circuits.Ising import initialize_Ising_circuit
-from DataPreprocessing import r
-# %%
-circuit = initialize_Ising_circuit()
+from Circuits.Ising import choose_n_qubits, initialize_Ising_circuit
+from DataPreprocessing import full_signal, r
+from CleanVersion.Functions.TrainingFuncs import *
+
+n_qubits = choose_n_qubits()
+
+circuit, weights = initialize_Ising_circuit(n_qubits)
+train, test, train_size, test_size = split_train_test(full_signal, n_qubits)
+final_train, final_test = scale_data(train, test, train_size, test_size, n_qubits)
+
+weights, x_t, target_y_t = train_model(final_train, final_test, weights, circuit, max_steps = 10, epochs = 10, bool_plot=True)
+
+save_weights(weights, 'TEST weights')
+save_test_data(x_t,target_y_t,'TEST test inputs', 'TEST test targets')
 # %%
