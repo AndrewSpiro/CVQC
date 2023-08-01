75bbd46a7551eccb38dd9c6750d2f563bbaee041
AndrewSpiro <spiro.ac@gmail.com>
2023-08-01 10:15:02 +0200

    Replace n_qubits function with argument and specify path for saves

--- CleanVersion/Training.py
+++ CleanVersion/Training.py
@@ -1,16 +1,14 @@
 # %%
-from Circuits.Ising import choose_n_qubits, initialize_Ising_circuit
-from DataPreprocessing import full_signal, r
+from Circuits.Ising import initialize_Ising_circuit
+from DataPreprocessing import full_signal
 from Functions.TrainingFuncs import *
 
-n_qubits = choose_n_qubits()
+circuit, weights, n_qubits = initialize_Ising_circuit(bool_draw = True)
 
-circuit, weights = initialize_Ising_circuit(n_qubits)
-train, test, train_size, test_size = split_train_test(full_signal, n_qubits)
-final_train, final_test = scale_data(train, test, train_size, test_size, n_qubits)
-
+train, test, train_size, test_size, train_ratio, shuffle_indices = split_train_test(full_signal, n_qubits)
+final_train, final_test, scaler = scale_data(train, test, train_size, test_size, n_qubits)
 weights, x_t, target_y_t = train_model(final_train, final_test, weights, circuit, max_steps = 10, epochs = 10, bool_plot=True)
 
-save_weights(weights, 'TEST weights')
-save_test_data(x_t,target_y_t,'TEST test inputs', 'TEST test targets')
+save_weights(weights, 'Results/TEST/TEST weights')
+save_test_data(x_t,target_y_t,'Results/TEST/TEST test inputs', 'Results/TEST/TEST test targets')
 # %%
