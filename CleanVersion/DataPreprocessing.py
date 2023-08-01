feb52c0ef6c2cf1e9ce73520fca06110209b8199
AndrewSpiro <spiro.ac@gmail.com>
2023-08-01 10:16:40 +0200

    Rename FT_mod_squared to find_components and add ability to import saved DC

--- CleanVersion/DataPreprocessing.py
+++ CleanVersion/DataPreprocessing.py
@@ -3,6 +3,6 @@ from Functions.DataPreprocessingFuncs import *
 # %%
 dataset = load_data('Datasets/AAPLmax.csv',usecols=['Close'], sample_size=201)
 percent_of_change = gradient(dataset)
-DC, amp, N = FT_mod_squared(percent_of_change, 1)
-full_signal, r = build_signal(DC, amp, 0, 0, N)
+DC, amp, N = find_components(load_PSD='Results/TEST/Apple 200 1.npy')
+full_signal, r = build_signal(DC, amp, 0, 0, N, bool_plot=True)  # Add plot labels
 # %%
