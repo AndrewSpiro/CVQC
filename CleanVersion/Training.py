914933ac43f47c152b41b39f01d6db77e2b4995e
AndrewSpiro <spiro.ac@gmail.com>
2023-08-04 14:27:22 +0200

    Update to last form of Training.py before implementing jax

--- CleanVersion/Training.py
+++ CleanVersion/Training.py
@@ -5,10 +5,10 @@ from Functions.TrainingFuncs import *
 
 circuit, weights, n_qubits = initialize_Ising_circuit(bool_draw = True)
 
-train, test, train_size, test_size, train_ratio, indices = split_train_test(full_signal, n_qubits)
+train, test, train_size, test_size, train_ratio, indices = split_train_test(full_signal, n_qubits, random = True)
 final_train, final_test, scaler = scale_data(train, test, train_size, test_size, n_qubits)
 # %%
-weights, x_t, target_y_t = train_model(final_train, final_test, weights, circuit, n_qubits, max_steps = 10, epochs = 10, bool_plot=True)
+weights, x_t, target_y_t = train_model(final_train, final_test, weights, circuit, n_qubits, max_steps = 10, epochs = 100, bool_plot=True, save_plot='Results/TEST/TEST loss')
 # %%
 results_and_params = {
     "n_qubits" : n_qubits,
