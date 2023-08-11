7380b168f6363a215c2a3d7f8e1b0ceb88a71fc8
AndrewSpiro <spiro.ac@gmail.com>
2023-08-11 15:09:22 +0200

    update to working jax

--- CleanJax/Testing.py
+++ CleanJax/Testing.py
@@ -11,10 +11,14 @@ weights = dict.get("weights")
 inputs = dict.get("inputs")
 targets = dict.get("targets")
 
-_, circuit, _, n_qubits = initialize_Ising_circuit(bool_draw = True)
+vcircuit, circuit, _, n_qubits = initialize_Ising_circuit(bool_draw = True)
+print(1)
+vpredictions, vtargets, vmse, vforward_mse =test(vcircuit, scaled_inputs = inputs, scaled_targets=targets, scaler = scaler, weights = weights, bool_scaled = True, vmapped = True)
+plot(vpredictions, vtargets, indices, n_qubits, bool_plot = True, save_plot=None, mse = vmse, forward_mse=vforward_mse, plot_labels=['Day','Percent of Change'])
 
 #circuit = load_circuit('Results/TEST/TEST circuit')
 # %%
+print(2)
 predictions, targets, mse, forward_mse =test(circuit, scaled_inputs = inputs, scaled_targets=targets, scaler = scaler, weights = weights, bool_scaled = True)
-plot(predictions, targets, indices, n_qubits, bool_plot = True, save_plot= 'Results/TEST/TEST predictions JAX', mse = mse, forward_mse=forward_mse, plot_labels=['Day','Percent of Change'])
+plot(predictions, targets, indices, n_qubits, bool_plot = True, save_plot= None, mse = mse, forward_mse=forward_mse, plot_labels=['Day','Percent of Change'])
 # %%
