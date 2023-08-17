3e0f6ef2f9f73e782d4d11256a56dc382c3fa29b
AndrewSpiro <spiro.ac@gmail.com>
2023-08-17 17:29:10 +0200

    Update docstrings

--- CleanJax/Functions/TrainingFuncs.py
+++ CleanJax/Functions/TrainingFuncs.py
@@ -9,16 +9,16 @@ from jax import numpy as jnp
 import os
 
 
-def split_train_test(data, n_qubits: int, train_ratio = 2/3, random = False, seed = 0):
+def split_train_test(data, n_qubits: int, train_ratio: float = 2/3, random: bool = False, seed: int = 0):
     '''
     Splits data into two sub-datsets: one for training and one for testing. 
     
             Parameters:
-                    data: 1-D data to be split
-                    n_qubits (int): number of qubits in the PQC
-                    train_ratio: fraction of full data to be used for training. Must be less than 1.
+                    data (numpy.array): 1-D data to be split
+                    n_qubits (int): number of qubits in the circuit
+                    train_ratio (float): fraction of full data to be used for training. Must be less than 1.
                     random (bool): if False (default), the data is split chronologically e.g., the first 2/3 of the data are used for training and the last 1/3 are used for testing. If True, the data is split randomly.
-                    seed (Any): seed for np.random
+                    seed (int): seed for np.random
             Returns:
                     train (numpy.ndarray): 2-D array containing the training data. The shape of the data is (train_size//n_qubits, n_qubits)
                     test (numpy.ndarray): 2-D array containing the testing data. The shape of the data is (test_size//n_qubits, n_qubits)
@@ -28,6 +28,8 @@ def split_train_test(data, n_qubits: int, train_ratio = 2/3, random = False, see
                     indices: gives the index of each "group" in the original data. If random is False, indices is just the range of integers from 0 to the number of groups.
     '''
     
+    # This transforms the data, creating subgroups of n_qubits consecutive elements each. The first group contains the first n_qubits elements, the second contains the second n_qubits elements, and so on.
+    # For example, if the data is [1,2,3,4,5,6] and n_qubits is 3, this returns [[1,2,3],[2,3,4],[3,4,5],[4,5,6]]
     grouped_data = np.zeros((len(data)-n_qubits+1, n_qubits))
     for i in range(len(data)-n_qubits+1):
         grouped_data[i] = (np.array(data[i:i+n_qubits])).squeeze()
@@ -46,21 +48,21 @@ def split_train_test(data, n_qubits: int, train_ratio = 2/3, random = False, see
     
     return train, test, train_size, test_size, train_ratio, indices
     
-def scale_data(train, test, train_size, test_size, n_qubits, scaler_min = 0.2, scaler_max = 0.8):
+def scale_data(train, test, train_size: int, test_size: int, n_qubits: int, scaler_min : float = 0.2, scaler_max : float = 0.8):
     '''
-    Scales the data for embedding in the PQC. By default, scales all values to be between 0.2 and 0.8.
+    Scales the data for embedding in the circuit. By default, scales all values to be between 0.2 and 0.8.
     
             Parameters:
-                    train (numpy.ndarray): 2-D array containing the training data. The shape of the data is (train_size//n_qubits, n_qubits)
-                    test (numpy.ndarray): 2-D array containing the testing data. The shape of the data is (test_size//n_qubits, n_qubits)
-                    train_size: the total number of values in train divided by n_qubits (i.e., the number of n_qubits-sized "groups" in train)
-                    test_size: the total number of values in test divided by n_qubits (i.e., the number of n_qubits-sized "groups" in test)
-                    n_qubits: number of qubits in the PQC
-                    scaler_min = the target minimum value for all values in the full dataset
-                    scaler_max = the target maximum value for all values in the full dataset
+                    train (numpy.ndarray): 2-D array containing the training data. The shape of the data is (train_size, n_qubits)
+                    test (numpy.ndarray): 2-D array containing the testing data. The shape of the data is (test_size, n_qubits)
+                    train_size (int): the length of the training set
+                    test_size (int): the length of the training set
+                    n_qubits (int): number of qubits in the circuit
+                    scaler_min (float)= the target minimum value for all values in the full dataset
+                    scaler_max (float)= the target maximum value for all values in the full dataset
             Returns:
-                    final_train (numpy.ndarray): 2-D array containing the scaled training data. The shape of the data is (train_size//n_qubits, n_qubits)
-                    final_test (numpy.ndarray): 2-D array containing the scaled testing data. The shape of the data is (test_size//n_qubits, n_qubits)
+                    final_train (numpy.ndarray): 2-D array containing the scaled training data. The shape of the data is (train_size, n_qubits)
+                    final_test (numpy.ndarray): 2-D array containing the scaled testing data. The shape of the data is (test_size, n_qubits)
                     scaler: The initialized and fitted scaler                    
     '''    
     init_scaler = MinMaxScaler((scaler_min,scaler_max))
@@ -78,14 +80,14 @@ def scale_data(train, test, train_size, test_size, n_qubits, scaler_min = 0.2, s
     
     return final_train, final_test, scaler
 
-def train_model(train, test, weights, circuit, n_qubits: int, max_steps: int, epochs: int, loss_function = 'square_loss', optimizer = 'qml.AdamOptimizer' , learning_rate = 0.1, bool_plot = False, save_plot: str = None):
+def train_model(train, test, weights, circuit, n_qubits: int, max_steps: int, epochs: int, loss_function = 'mean square error', learning_rate = 0.1, bool_plot = False, save_plot: str = None):
     '''
-    Trains the PQC using the "train" dataset and according to the specified hyperparameters. 
+    Trains the circuit using the "train" dataset and according to the specified hyperparameters. 
     
             Parameters:
                     train (numpy.ndarray): 2-D array containing the scaled training data. Used to train the model and update weights.
                     test (numpy.ndarray): 2-D array containing the scaled testing data. Used to check progress of training.
-                    weights: parameters of the PQC updated by the optimizer to change output.
+                    weights: parameters of the circuit updated by the optimizer to change output.
                     circuit (pennylane.qnode.QNode): The circuit which encodes the training data and, according to its parameters, returns an output to be compared with "target" data
                     n_qubits (int): The number of qubits in the quantum circuit
                     max_steps (int): The number of times the weights should be updated before the entire dataset has been evaluated. 
@@ -119,27 +121,44 @@ def train_model(train, test, weights, circuit, n_qubits: int, max_steps: int, ep
             target_y[i] = data[i][-1]
         return x, target_y
     
-    valid_loss_functions = ["square_loss"]
+    valid_loss_functions = ["mean square error"]
     if loss_function not in valid_loss_functions:
-        raise ValueError("Invalid loss function! Only 'square_loss' is allowed.")
-    elif loss_function == "square_loss":
+        raise ValueError("Invalid loss function! Only currently supported loss function is 'mean square error'")
+    elif loss_function == "mean square error":
         def square_loss(targets, predictions):
+            """
+                Calculate the mean squared error (MSE) between target values and predicted values.
+
+                        Parameters:
+                                targets: The true target values.
+                                predictions: The predicted values.
+
+                        Returns:
+                                DynamicJaxprTracer: The calculated mean squared error between targets and predictions.
+            """
             reshaped_predictions = predictions.reshape(len(predictions),1)
             return jnp.mean((targets - reshaped_predictions) ** 2)
         
         def not_jit_cost(weights, x, y):
+            """
+                Calculate the cost function by evaluating the mean squared error between predicted values and target values.
+
+                This function calculates predictions using the vmapped function 'circuit' and computes
+                the mean squared error between the predictions and true target values.
+
+                        Parameters:
+                                weights: Quantum circuit parameters.
+                                x (Jax tensor): Input data for making predictions.
+                                y (Jax tensor): True target values.
+
+                        Returns:
+                                DynamicJaxprTracer: Mean squared error between predicted values and true target values.
+            """
             predictions = circuit(weights, x)
             return square_loss(y, predictions)
         
+        # Create a jitted version of the cost function for just-in-time compilation
         cost = jax.jit(not_jit_cost)
-    
-    valid_optimizers = ['qml.AdamOptimizer', 'qml.AdagradOptimizer']
-    if optimizer not in valid_optimizers:
-        raise ValueError("Invalid loss function! Only 'square_loss' is allowed.")
-    elif optimizer == 'qml.AdamOptimizer':
-        opt = qml.AdamOptimizer(learning_rate)
-    elif optimizer == 'qml.AdagradOptimizer':
-        opt = qml.AdagradOptimizer(learning_rate)      
         
     x, target_y = input_target_split(train)
     x_t, target_y_t = input_target_split(test)
@@ -157,6 +176,7 @@ def train_model(train, test, weights, circuit, n_qubits: int, max_steps: int, ep
             batch_index = np.random.randint(0, max_steps, batch_size)
             x_batch = x[batch_index]
             y_batch = target_y[batch_index]
+            # Convert arrays into Jax tensors for vmapped circuit
             jax_x_batch = jnp.array(x_batch)
             jax_y_batch = jnp.array(y_batch)
             # Update the weights by one optimizer step
@@ -185,8 +205,15 @@ def train_model(train, test, weights, circuit, n_qubits: int, max_steps: int, ep
     
     return(weights, x_t, target_y_t)
 
-def save_results_params(results_and_params, dict_path):
-    if not os.path.exists(dict_path):
-        os.makedirs(dict_path)
-    with open(dict_path + 'dict', 'wb') as fp:
+def save_results_params(results_and_params, path: str):
+    '''
+            Saves a dictionary containing n_qubits, indices, scaler, weights, inputs, and targets to the specified path
+            
+                    Parameters:
+                            results_and_params (dict): A dictionary containing n_qubits, indices, scaler, weights, inputs, and targets
+                            path (str): path to which the dictionary will be saved as "dict"
+    '''
+    if not os.path.exists(path):
+        os.makedirs(path)
+    with open(path + 'dict', 'wb') as fp:
         pickle.dump(results_and_params, fp)
