7a81e7a56e3642f20875224d166719f36377ab1c
AndrewSpiro <spiro.ac@gmail.com>
2023-08-17 16:50:40 +0200

    Update docstrings

--- CleanJax/DataPreprocessing.py
+++ CleanJax/DataPreprocessing.py
@@ -1,8 +1,10 @@
-# %%
 from Functions.DataPreprocessingFuncs import *
-# %%
+
 dataset = load_data('Datasets/AAPLmax.csv',usecols=['Close'], sample_size=500)
 percent_of_change = gradient(dataset)
-DC, amp, N = find_components(load_PSD='Results/TEST/Apple 200 1.npy')
-full_signal, r = build_signal(DC, amp, 0, 0, N, bool_plot=True, labels = ['F0', 'Day', 'Percent of change'])  # Add plot labels
-# %%
+
+# Use load_PSD if there exists already a file with the power spectral density. Otherwise this functions calculates it.
+DC, amp, N = find_components(load_PSD='Results/TEST/Apple/Apple 200 1.npy')
+
+
+full_signal, r = build_signal(DC, amp, 0, 0, N, bool_plot=True, labels = ['Title', 'X-axis', 'Y-axis'])
