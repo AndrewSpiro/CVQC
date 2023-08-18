449df0025653a599a7193528cabb48758e4b7430
AndrewSpiro <spiro.ac@gmail.com>
2023-08-18 09:51:26 +0200

    Remove non-jax circuit

--- CleanJax/Training.py
+++ CleanJax/Training.py
@@ -3,15 +3,15 @@ from Circuits.MPS import initialize_MPS_circuit
 from DataPreprocessing import full_signal
 from Functions.TrainingFuncs import *
 
-loss_path = 'Results/TEST/nonexisting/'
-dict_path = 'Results/TEST/nonexisting2/'
+# Specify path using data, model and hyperparameters. For example: 'Results/Apple/Ising/100Epochs/'
+path = 'Results/Test/'
 
-circuit, _,weights, n_qubits = initialize_MPS_circuit()
+circuit, weights, n_qubits = initialize_MPS_circuit()
 
 train, test, train_size, test_size, train_ratio, indices = split_train_test(full_signal, n_qubits, random = True)
 final_train, final_test, scaler = scale_data(train, test, train_size, test_size, n_qubits)
 
-weights, x_t, target_y_t = train_model(final_train, final_test, weights, circuit, n_qubits, max_steps = 10, epochs = 10, bool_plot=True, save_plot=loss_path, learning_rate=0.1)
+weights, x_t, target_y_t = train_model(final_train, final_test, weights, circuit, n_qubits, max_steps = 10, epochs = 10, bool_plot=True, save_plot=path, learning_rate=0.1)
 
 results_and_params = {
     "n_qubits" : n_qubits,
@@ -22,4 +22,4 @@ results_and_params = {
     "targets" : target_y_t
 }
 
-save_results_params(results_and_params, dict_path)
+save_results_params(results_and_params, path)
