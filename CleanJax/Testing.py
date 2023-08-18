38f8ef653e17a15ba9a8386484e62da6e252cd41
AndrewSpiro <spiro.ac@gmail.com>
2023-08-18 09:48:27 +0200

    Remove vmapped parameter

--- CleanJax/Testing.py
+++ CleanJax/Testing.py
@@ -2,8 +2,10 @@ from Circuits.Ising import initialize_Ising_circuit
 from Circuits.MPS import initialize_MPS_circuit
 from Functions.TestingFuncs import * 
 
-dict_path = 'Results/FullApple/MPS/dict'
-predictions_path = 'Results/TEST/nonexisting/'
+dict_path = 'Results/Test/dict'
+
+# Specify path using data, model and hyperparameters. For example: 'Results/Apple/Ising/100Epochs/'
+predictions_path = 'Results/'
 
 dict = load_results_params(dict_path)
 n_qubits = dict.get("n_qubits")
@@ -15,5 +17,5 @@ targets = dict.get("targets")
 
 circuit, _, _, n_qubits = initialize_MPS_circuit()
 
-predictions, targets, mse, forward_mse =test(circuit, scaled_inputs = inputs, scaled_targets=targets, scaler = scaler, weights = weights, bool_scaled = True, vmapped = True)
+predictions, targets, mse, forward_mse =test(circuit, scaled_inputs = inputs, scaled_targets=targets, scaler = scaler, weights = weights, bool_scaled = True)
 plot(predictions, targets, indices, n_qubits, bool_plot = True, save_plot=predictions_path, mse = mse, forward_mse=forward_mse, plot_labels=['Day','Percent of Change'])
