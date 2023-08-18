a6749a37a94d3efaa1632a07168d7ae9806985a7
AndrewSpiro <spiro.ac@gmail.com>
2023-08-18 15:04:09 +0200

    Put all hyperparams in one place

--- CleanJax/DataPreprocessing.py
+++ CleanJax/DataPreprocessing.py
@@ -1,10 +1,18 @@
 from Functions.DataPreprocessingFuncs import *
 
-dataset = load_data('Datasets/AAPLmax.csv',usecols=['Close'], sample_size=500)
+Data = 'AAPLmax' # Provide the name of a csv file without the .csv extension
+sample_size = 200
+threshold = 1
+Noise = 0 # Coefficient of noise. Possible values are integers from 0 to 5 inclusive
+Trend = 0 # Trend with 0 corresponding to no trend, 1 corresponding to linear, and 2 corresponding to quadratic
+
+path = 'Results/' + Data + '/' + str(sample_size) + '/' + str(threshold) + '/'
+
+dataset = load_data('Datasets/' + Data + '.csv',usecols=['Close'], sample_size=sample_size)
 percent_of_change = gradient(dataset)
 
-# Use load_PSD if there exists already a file with the power spectral density. Otherwise this functions calculates it.
-DC, amp, N = find_components(load_PSD='Results/TEST/Apple/Apple 200 1.npy')
+# Provide load_PSD = path if there exists already a file with the power spectral density. Otherwise specify the parameters 'signal' and 'threshold' and provide parameter save_components = path.
+DC, amp, N = find_components(load_PSD=path)
 
 
-full_signal, r = build_signal(DC, amp, 0, 0, N, bool_plot=True, labels = ['Title', 'X-axis', 'Y-axis'])
+full_signal, r = build_signal(DC, amp, Noise, Trend, N, bool_plot=True, labels = ['Title', 'X-axis', 'Y-axis'])
