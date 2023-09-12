68418bc412afc1f8c512aa457ab9db8b25a708ff
AndrewSpiro <77333606+AndrewSpiro@users.noreply.github.com>
2023-09-12 18:40:36 +0200

    add parent directories to two paths

--- src/DataPreprocessing.py
+++ src/DataPreprocessing.py
@@ -6,10 +6,10 @@ threshold = 1
 Noise = 0  # Coefficient of noise. Possible values are integers from 0 to 5 inclusive
 Trend = 0  # Trend with 0 corresponding to no trend, 1 corresponding to linear, and 2 corresponding to quadratic
 
-path = "Results/" + Data + "/" + str(sample_size) + "/" + str(threshold) + "/"
+path = "CVQC/src/Results/" + Data + "/" + str(sample_size) + "/" + str(threshold) + "/"
 
 dataset = load_data(
-    "Datasets/" + Data + ".csv", usecols=["Close"], sample_size=sample_size
+    "CVQC/src/Datasets/" + Data + ".csv", usecols=["Close"], sample_size=sample_size
 )
 percent_of_change = gradient(dataset)
 
@@ -18,5 +18,5 @@ DC, amp, N = find_components(load_PSD=path)
 
 
 full_signal, r = build_signal(
-    DC, amp, Noise, Trend, N, bool_plot=True, labels=["Title", "X-axis", "Y-axis"]
+    DC, amp, Noise, Trend, N, bool_plot=False, labels=["Title", "X-axis", "Y-axis"]
 )
