0a9da2f69fbc1c5b917220fc6155b0d526cf82f2
AndrewSpiro <77333606+AndrewSpiro@users.noreply.github.com>
2023-09-13 11:23:07 +0200

    update DataPreprocessing.py

--- src/DataPreprocessing.py
+++ src/DataPreprocessing.py
@@ -9,12 +9,13 @@ Trend = 0  # Trend with 0 corresponding to no trend, 1 corresponding to linear, 
 # path = "Results/" + Data + "/" + str(sample_size) + "/" + str(threshold) + "/"
 path = "C:\Code\CVQC\src\Results\AAPLmax\\200\\1\\"
 
-
 dataset = load_data(
     # "Datasets/" + Data + ".csv", usecols=["Close"], sample_size=sample_size
-    "C:\Code\CVQC\src\Datasets\AAPLmax.csv", usecols=["Close"], sample_size=sample_size
-
+    r'C:/Code/CVQC/src/Datasets/AAPLmax.csv',  # Use forward slashes or double backslashes
+    usecols=["Close"],
+    sample_size=sample_size
 )
+
 percent_of_change = gradient(dataset)
 
 # Provide load_PSD = path if there exists already a file with the power spectral density. Otherwise specify the parameters 'signal' and 'threshold' and provide parameter save_components = path.
